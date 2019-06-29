adbc54892139f6ea729e2539f76fb09e352c6318
inyeongkim <hacerrumbo@gmail.com>
2019-06-29 23:19:37 +0900

    Add on MAC

--- chap3/3.6.1_mnist_practice.py
+++ chap3/3.6.1_mnist_practice.py
@@ -1,10 +1,27 @@
+import numpy as np
 import sys, os
 sys.path.append(os.pardir)
 from dataset.mnist import load_mnist
+from PIL import Image
 
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show()
+
+# load_mnist retrun type
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
 
-print(x_train.shape)
-print(t_train.shape)
-print(x_test.shape)
-print(t_test.shape)
+img = x_train[311]
+label = t_train[0]
+print(label)
+
+print(img.shape)
+img = img.reshape(28, 28)   # original size (28*28)
+print(img.shape)
+
+img_show(img)
+
+# print(x_train.shape)
+# print(t_train.shape)
+# print(x_test.shape)
+# print(t_test.shape)
